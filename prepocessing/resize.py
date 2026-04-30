0a6860f7ebd517d9c2d693ecb6d6f183bfa8305c
TheodorusLimbong <theodoruslimbong@gmail.com>
2026-04-30 17:34:01 +0700

    resize done

--- prepocessing/resize.py
+++ prepocessing/resize.py
@@ -3,3 +3,13 @@ from torchvision import transforms
 
 def get_resize_transform(img_size=256):
     return transforms.Resize((img_size, img_size))
+if __name__ == "__main__":
+    from PIL import Image
+
+    img = Image.open("dataset/mvtec_anomaly_detection/bottle/train/good/000.png")
+
+    transform = get_resize_transform(256)
+    resized = transform(img)
+
+    print("Original size:", img.size)
+    print("Resized size:", resized.size)
