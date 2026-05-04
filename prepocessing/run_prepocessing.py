03be64e5cb3243d4c340c62b7adb743ba5f82f78
TheodorusLimbong <theodoruslimbong@gmail.com>
2026-05-04 15:02:13 +0700

    rp rev

--- prepocessing/run_prepocessing.py
+++ prepocessing/run_prepocessing.py
@@ -4,37 +4,19 @@ from preprocessing.dataloader import get_dataloader
 def run_preprocessing():
     root_dir = "dataset/mvtec_anomaly_detection"
 
-    print("===== PREPROCESSING START =====")
+    train_loader = get_dataloader(root_dir, "train")
+    test_loader = get_dataloader(root_dir, "test")
 
-    train_loader = get_dataloader(
-        root_dir=root_dir,
-        category="bottle",
-        phase="train",
-        batch_size=8
-    )
+    print("===== PREPROCESSING CHECK =====")
 
-    test_loader = get_dataloader(
-        root_dir=root_dir,
-        category="bottle",
-        phase="test",
-        batch_size=8
-    )
-
-    # ===== CEK SAMPLE =====
     train_sample = next(iter(train_loader))
     test_sample = next(iter(test_loader))
 
-    print("\n--- TRAIN SAMPLE ---")
-    print("Shape:", train_sample["image"].shape)
-    print("Label:", train_sample["label"])
+    print("\nTRAIN:")
+    print(train_sample["image"].shape, train_sample["label"])
 
-    print("\n--- TEST SAMPLE ---")
-    print("Shape:", test_sample["image"].shape)
-    print("Label:", test_sample["label"])
-
-    print("\n===== PREPROCESSING DONE =====")
-
-    return train_loader, test_loader
+    print("\nTEST:")
+    print(test_sample["image"].shape, test_sample["label"])
 
 
 if __name__ == "__main__":
