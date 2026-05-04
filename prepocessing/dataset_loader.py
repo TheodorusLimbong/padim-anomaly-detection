41c8de99800584c44219e99ad2b7ebd8d6d61510
TheodorusLimbong <theodoruslimbong@gmail.com>
2026-05-04 15:01:06 +0700

    dl rev

--- prepocessing/dataset_loader.py
+++ prepocessing/dataset_loader.py
@@ -1,31 +1,12 @@
-from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
-from src.config import IMAGE_SIZE, BATCH_SIZE, NUM_WORKERS
+from preprocessing.dataset_wrapper import MVTecDataset
 
-def get_transforms(train=True):
-    if train:
-        return transforms.Compose([
-            transforms.Resize((IMAGE_SIZE, IMAGE_SIZE)),
-            transforms.RandomHorizontalFlip(),
-            transforms.ToTensor()
-        ])
-    else:
-        return transforms.Compose([
-            transforms.Resize((IMAGE_SIZE, IMAGE_SIZE)),
-            transforms.ToTensor()
-        ])
 
-def load_dataset(path, train=True):
-    dataset = datasets.ImageFolder(
-        root=path,
-        transform=get_transforms(train)
-    )
-    
-    loader = DataLoader(
+def get_dataloader(root_dir, phase="train", batch_size=8):
+    dataset = MVTecDataset(root_dir=root_dir, phase=phase)
+
+    return DataLoader(
         dataset,
-        batch_size=BATCH_SIZE,
-        shuffle=train,
-        num_workers=NUM_WORKERS
+        batch_size=batch_size,
+        shuffle=(phase == "train")
     )
-    
-    return loader
